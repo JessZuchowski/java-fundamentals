bacda30017c9d4696702f40eb992bac41f3901fe
jaezuchowski <jess.zuchowski@gmail.com>
2019-04-21 12:41:57 -0700

    working on clock method

--- basiclibrary/src/basics/Main.java
+++ basiclibrary/src/basics/Main.java
@@ -2,13 +2,17 @@ public class Main {
   public static void main(String[] args) {
     //pluralize test 
     System.out.println(pluralize("dog", 3));
+    System.out.println(pluralize("cat", 1));
+    System.out.println(pluralize("turtle", 5));
     //flipNHeads test
-    // double flip = Math.random();
-    // System.out.println("random flip:"+ flip);
     System.out.println(flipNHeads(3));
+    System.out.println(flipNHeads(5));
+    System.out.println(flipNHeads(7));
+    //clock test
+    clock();
   }
+
   //write a function called pluralize that accepts a word and a number and returns a string with the word pluralized with an 's' if the number is 0 or >1.
-  
   public static String pluralize(String word, int num) {
     if (num == 0 || num >1) {
       return word + "s ";  
@@ -23,8 +27,7 @@ public class Main {
     random = Math.random();
     do { for (int j = 0; j < num; j++) {
       i++;
-      random = (double)Math.random(); 
-      // System.out.println("do");
+      random = (double)Math.random();
       if (random <= 0.5) {
         System.out.println("tails");
       } else {
@@ -33,9 +36,17 @@ public class Main {
     } 
     }
     while (i < num);
-    // System.out.println("while");
       return num;
   }
 
   // write a method clock that uses LocalDateTime object to constantly print current time to console each second. Should run until manually killed. should detect when seconds increase and only print when timestamp changes.
+  public static String clock() {
+    LocalDateTime now = new LocalDateTime.now();
+    int hour = now.getHour();
+    int minute = now.getMinute();
+    int second = now.getSecond();
+    String time = now.format(DateTimeFormatter.ofPattern("HH:mm:ss"));
+    System.out.println(time);
+    return time;
+  }
 }
